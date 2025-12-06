055433b1ad943a72212621433c51a747889c18e9
Surendra <surendrasaluru@gmail.com>
2025-12-06 20:59:22 +0530

    first

---
+++ src/data/products.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: 1,
+    name: "Wireless Earbuds Pro",
+    price: 999.0,
+  },
+  {
+    id: 2,
+    name: "SmartWatch Elite",
+    price: 1999.0,
+  },
+  {
+    id: 3,
+    name: "Premium Bluetooth Speaker",
+    price: 2200.5,
+  },
+  {
+    id: 4,
+    name: "Ergonomic Laptop Stand",
+    price: 4900.2,
+  },
+  {
+    id: 5,
+    name: "Nothing 2A",
+    price: 21000.2,
+  },
+  {
+    id: 6,
+    name: "Realme Narzo 50",
+    price: 17000.2,
+  },
+  {
+    id: 7,
+    name: "Oneplus Nord CE2 Lite",
+    price: 19000.4,
+  },
+  {
+    id: 8,
+    name: "Samsung M20 Pro",
+    price: 32000.29,
+  },
+];
